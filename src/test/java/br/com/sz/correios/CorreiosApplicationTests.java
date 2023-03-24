6921ad001f173ee20e350fcbce55f80e17cf5c7a
Samuel <samuel.estudo778@gmail.com>
2023-03-24 10:16:26 -0300

    chore: initial settings

---
+++ src/test/java/br/com/sz/correios/CorreiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sz.correios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
